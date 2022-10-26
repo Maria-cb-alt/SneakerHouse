2a3a7a54c81e0f26f2101fdaffe98a2c3e9c203f
Carol <carolzinhabalassoni@gmail.com>
2022-10-26 01:21:46 -0300

    commit para teste

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 
 const app = express();
 
+const name = maria;
+
 app.listen(3080, ()=>{
     console.log('Servidor rodando em: http://localhost:3080');
 
 });
 
 const db = require('./database/database'); 
+
+console.log(name);
